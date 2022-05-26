258f140d149769dce5de4b4a0e471963de0231f2
Julio Teixeira <jcesarprog@gmail.com>
2022-05-26 17:02:42 -0300

    first commit+

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCircleMinus,faCirclePlus,faCartShopping} from "./icons"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <p>Test</p>
+
+      </div>
     </div>
   );
 }
---
+++ src/icons/index.ts
@@ -0,0 +1,13 @@
+export {
+    faCircleMinus,
+    faCirclePlus, 
+    faCartShopping, // a shopping cart
+  } from "@fortawesome/free-solid-svg-icons";
+
+
+  /*
+<FontAwesomeIcon
+          icon={faCircleMinus}
+          style={{ fontSize: 100, color: "blue", marginRight: 30 }}
+        />
+  */
